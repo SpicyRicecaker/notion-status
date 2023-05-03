0d1eb4fe123d4153bfff8f5439cbd0deec83e914
SpicyRicecaker <awesomenessisgolden@gmail.com>
2023-05-02 18:14:10 -0700

    remove semicolons

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { app, BrowserWindow } from "electron";
-import * as path from "path";
+import { app, BrowserWindow } from "electron"
+import * as path from "path"
 
 function createWindow() {
   // Create the browser window.
@@ -8,19 +8,21 @@ function createWindow() {
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
-      nodeIntegration: true
+      nodeIntegration: true,
     },
     alwaysOnTop: true,
     transparent: true,
     frame: false,
-    // color format is in #AARRGGBB 
+    // color format is in #AARRGGBB
     // keep in mind taht this value doesn't matter so long as transparent is
     // true
-    backgroundColor: '#00000000'
-  });
+    backgroundColor: "#00000000",
+  })
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, "../index.html"));
+
+  // load the development server in development mode, otherwise load the built version in production
+  mainWindow.loadFile(path.join(__dirname, "../index.html"))
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools();
@@ -30,23 +32,23 @@ function createWindow() {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow();
+  createWindow()
 
   app.on("activate", function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
--- src/preload.ts
+++ src/preload.ts
@@ -2,13 +2,16 @@
 // It has the same sandbox as a Chrome extension.
 window.addEventListener("DOMContentLoaded", () => {
   const replaceText = (selector: string, text: string) => {
-    const element = document.getElementById(selector);
+    const element = document.getElementById(selector)
     if (element) {
-      element.innerText = text;
+      element.innerText = text
     }
-  };
+  }
 
   for (const type of ["chrome", "node", "electron"]) {
-    replaceText(`${type}-version`, process.versions[type as keyof NodeJS.ProcessVersions]);
+    replaceText(
+      `${type}-version`,
+      process.versions[type as keyof NodeJS.ProcessVersions]
+    )
   }
-});
+})
